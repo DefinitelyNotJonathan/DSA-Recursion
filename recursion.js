a63de8657251d7e9af584593cd2ff9984f8a21a1
Jonathan Retchless <jonretchless@gmail.com>
2020-07-30 20:37:17 -0700

    maze work

--- recursion.js
+++ recursion.js
@@ -106,7 +106,182 @@ maze(0,0,[
     [' ', ' ', ' ', ' ', ' ', ' ', 'e']
 ]);
 
+function escapeMazeExhaustive(previousPositions, maze, direction){
+    //console.log('entering with numpositions: ',previousPositions.length)
+    //each iteration returns an array of arrays, all possible sets of future moves
+    //console.log('entered with ',previousPositions)
+    let latestPos=previousPositions[previousPositions.length-1]
+    if(direction){
 
+        let newPosition=null
+        if(direction==="L"){
+            if(latestPos.x < 1){
+                console.log('end condition cannot move left');
+                return null;
+            }
+            else{
+                newPosition={x:latestPos.x-1, y:latestPos.y, direction:'L'};
+            }
+        }
+        if(direction==="R"){
+            //console.log('latestPos: ',latestPos)
+            if(latestPos.x >= maze[latestPos.y].length-1){
+                console.log('end condition cannot move right');
+                return null;
+            }
+            else{
+                newPosition={x:latestPos.x+1, y:latestPos.y, direction:'R'}
+            }
+        }
+        if(direction==="U"){
+            if(latestPos.y < 1){
+                console.log('end condition cannot move up');
+                return null;
+            }
+            else{
+                newPosition={x:latestPos.x, y:latestPos.y-1, direction:'U'}
+            }
+        }
+        if(direction==="D"){
+            if(latestPos.y >= maze.length-1){
+                console.log('end condition cannot move down');
+                return null;
+            }
+            else{
+                newPosition={x:latestPos.x, y:latestPos.y+1, direction:'D'}
+            }
+        }
+
+        if(previousPositions.find(function(item){
+            return item.x === newPosition.x && item.y === newPosition.y
+        })){
+            console.log('end condition, moving to a previous position')
+            return null
+        }
+        //return an array of all possible future positions, that can be concatd with current array of positions
+        
+        if(maze[newPosition.y][newPosition.x] === '*'){
+            console.log('end condition, trying to move into wall')
+            return null
+        }
+        
+        if(newPosition.x===maze[0].length-1 && newPosition.y===maze.length-1){
+            console.log('end condition found maze end')
+            return [[latestPos,newPosition]];
+        }
+        else{
+            console.log('SUCCESS FOR DIRECTION: ',newPosition.direction)
+            let results=[];
+            let newPositions=previousPositions.concat([newPosition]);
+            console.log('newPositions is: ',newPositions)
+            results.push(escapeMazeExhaustive(newPositions,maze,'L'));
+            results.push(escapeMazeExhaustive(newPositions,maze,'R'));
+            results.push(escapeMazeExhaustive(newPositions,maze,'U'));
+            results.push(escapeMazeExhaustive(newPositions,maze,'D'));
+            
+            let branches=[];
+            for(let i=0; i<results.length; i++){
+                //console.log('result[i] is: ',results[i]);
+                if(results[i]){//array of sets of future positions
+                    for(let x=0; x<results[i].length; x++){//an array of future positions
+                        //console.log('result[i][x] is: ',results[i][x]);
+                        if(results[i][x]){
+                            let merged=[latestPos].concat(results[i][x]);
+                            //console.log('merged is ',merged)
+                            branches.push(merged);
+                        }
+                    }
+                }
+            }
+
+            return branches.length ? branches : null;
+        }
+    }
+    else{
+        let results=[];
+        results.push(escapeMazeExhaustive(previousPositions,maze,'L'));
+        results.push(escapeMazeExhaustive(previousPositions,maze,'R'));
+        results.push(escapeMazeExhaustive(previousPositions,maze,'U'));
+        results.push(escapeMazeExhaustive(previousPositions,maze,'D'));
+        
+        for(let i=0; i<results.length; i++){
+            if(results[i]){
+                let branch=[latestPos].concat(results[i]);
+                /*
+                let invalid=false;
+                for(let i=0; i<branch.length; i++){
+                    if(branch.find(function(item,index){
+                        return (index != i && item.x === branch[i].x && item.y === branch[i].y)
+                    })){
+                        invalid=true;
+                    }
+                }
+                */
+                if(1){//!invalid){
+                    for(let i=0; i<branch.length; i++){
+                        let string='';
+                        for(let x=0; x<branch[i].length; x++){
+                            string+=branch[i][x].direction;
+                        }
+
+                        console.log(string);
+                    }
+                }
+                else{
+                    console.log('found one that is invalid, should not happen')
+                }
+            }
+        }
+        console.log('results',JSON.stringify(results))
+        
+    }
+}
+/*
+[' ', ' ', ' ', '*', ' ', ' ', ' '],
+['*', '*', ' ', '*', ' ', '*', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', ' '],
+[' ', '*', '*', '*', '*', '*', ' '],
+[' ', ' ', ' ', ' ', ' ', ' ', 'e']
+RRDDLLDDRRRRR
+RRDDRRRRD
+RRDDRRUURRDDD
+*/
+console.log('result of maze is: ',escapeMazeExhaustive([{x:0,y:0,direction:''}],maze,''));
+
+function createAnagrams(string){
+    let results=[];
+    if(string.length === 1){
+        return [string];
+    }
+    
+    for(let i=0; i<string.length; i++){
+        let strarr=string.split('');
+        let removed=strarr.splice(i,1);
+        let result=createAnagrams(strarr.join(''));
+        for(let x=0; x<result.length; x++){
+            results.push(removed+result[x])
+        }
+    }
+    
+    return results;
+}
+console.log(createAnagrams("asd"));
+function binaryRepresentation(num,str){
+    if(num<=0){
+        return str;
+    }
+    let bit=Math.floor(Math.log2(num))
+    let strlen=str.length;
+    for(let i=0; i < (bit+1)-strlen; i++){  
+        str+='0';
+    }
+    console.log('num,bit',num,bit,str);
+    str=str.slice(0,str.length-1-(bit))+'1'+str.slice(str.length-1-(bit)+1);
+    console.log('str after',str)
+    num-=2**bit;
+    return binaryRepresentation(num,str);
+}
+console.log(binaryRepresentation(257,''));
 function allWaysOut() {
 //take starting position, move left, right, up, down, call function
 //each of those will move left, right, up, down again
